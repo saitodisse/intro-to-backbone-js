06a16c8c8b901765c33d09c12f6740cc67bce5df
Julio Makdisse Saito <saitodisse@gmail.com>
2013-12-02 20:06:36 -0200

    only demo

--- Gemfile
+++
@@ -1,8 +0,0 @@
-source :rubygems
-
-gem 'showoff'
-gem 'foreman'
-
-group :development do
-  gem 'heroku'
-end
--- ngauthier.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-  if (window.location.search.indexOf('nodeadzone') > -1) {
-    var topStyle = $('<style></style>');
-    topStyle.text(".slide .content { margin-top: 20px !important }");
-    $('head').append(topStyle);
-  }
-});
